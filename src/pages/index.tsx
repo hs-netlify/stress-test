1029f960c3e6e4c115126748e2a1e9c5699e7ee2
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2023-05-23 13:42:28 +0200

    Make index page server side rendered

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-	return <div>Home page</div>;
+import { InferGetServerSidePropsType } from "next";
+
+export function getServerSideProps() {
+	return {
+		props: {
+			randomNumber: Math.random(),
+		},
+	};
+}
+
+export default function Home({
+	randomNumber,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+	return <div>{randomNumber}</div>;
 }
